0cea431c276c0bea111a3355126d626a2edca1ab
Brenim <bsampaio8@hotmail.com>
2022-11-16 21:05:11 -0300

    adiciona dependencias no docker

--- setup/settings.py
+++ setup/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bym',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
