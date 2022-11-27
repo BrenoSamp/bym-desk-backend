461211c828bff19904de5c3d26968f7c10fb5083
Matheus Henriques <math.marqui@gmail.com>
2022-11-27 15:42:46 -0300

    Criação de ticket

--- setup/urls.py
+++ setup/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from bym_desk_app.views import UsuariosViewSet, AnalistasViewSet, TicketsViewSet, ListaTicketsUsuarioViewSet, createAnalista, MensagensViewSet, ListaMensagensTicketViewSet, BlocosViewSet, LocaisViewSet, login, createUser, listTicketsAnalista, listTicketsSolicitante, MatriculasViewSet
+from bym_desk_app.views import UsuariosViewSet, AnalistasViewSet, TicketsViewSet, ListaTicketsUsuarioViewSet, createAnalista, MensagensViewSet, ListaMensagensTicketViewSet, BlocosViewSet, LocaisViewSet, login, createUser, listTicketsAnalista, listTicketsSolicitante, MatriculasViewSet, createTicket
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -35,5 +35,6 @@ urlpatterns = [
     path('analista/create', createAnalista),
     path('solicitante/tickets', listTicketsSolicitante),
     path('solicitante/create', createUser),
+    path('solicitante/criarTicket', createTicket),
     path('solicitante/<int:ticket_id>/mensagens', ListaMensagensTicketViewSet.as_view()),
 ]
