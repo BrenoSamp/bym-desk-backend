a635751b672a53c3b9c5c2d0800231f56dd8c816
Brenim <bsampaio8@hotmail.com>
2022-12-05 23:45:40 -0300

    correcao cors

--- setup/settings.py
+++ setup/settings.py
@@ -27,9 +27,11 @@ SECRET_KEY = 'django-insecure-y97tml^2mq5xd^d0-y#n$-te^7_@n1qrv!3k*$scv48%u5k$%-
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
